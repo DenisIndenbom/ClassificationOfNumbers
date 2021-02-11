8e4b9e8960be989e3a80dd1baefd425f3af168ff
DenisIndenbom <59681620+DenisIndenbom@users.noreply.github.com>
2021-02-11 15:08:48 +0300

    Update classification_of_numbers.py

--- classification_of_numbers.py
+++ classification_of_numbers.py
@@ -1,8 +1,11 @@
 import pygame
+print("loading..")
 import keyboard as kb
 import tensorflow as tf
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+
 
 model = tf.keras.models.load_model('model.h5')
 model.summary()
@@ -10,11 +13,13 @@ model.summary()
 pygame.init()
 screen = pygame.display.set_mode((128*3,128*3))
 screen.fill((0, 0, 0))
-pygame.display.set_caption("NNPaint")
+pygame.display.set_caption("ClassificationOfNumbers")
 clock = pygame.time.Clock()
 draw_on = False
-radius = 20
-is_predict = False
+radius = 15
+is_predict = True
+
+
 
 while True:
     e = pygame.event.wait()
@@ -36,18 +41,21 @@ while True:
     if not draw_on and not is_predict:
         pygame.image.save(screen,"temp.png")
         load_img = cv2.imread( 'temp.png' )
+        #img = cv2.resize( load_img, (128*10, 128*10) )
         img = cv2.resize( load_img, (28, 28) )
         img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        #plt.figure( figsize=(5, 5))
-        #plt.imshow(img)
+        show_img = img
         img = (np.expand_dims( img, 0 ))
         prediction = model.predict(img)
         print(prediction)
         val = 0
         for i,row in enumerate(prediction[0]):
-            if row > val <= 1:
+            if row > val:
                 val = row
                 pygame.display.set_caption(f"Это цифра: {i}")
-                break
+
         is_predict = True
+        # plt.figure( figsize=(5, 5) )
+        # plt.imshow( show_img )
+        # plt.show()
     pygame.display.flip()
